c1c3af4945dddfa5a5ca4812edbb9fd814265985
victorcabeceira <victorfgcabeceira@gmail.com>
2014-08-25 19:17:23 -0300

    Indentacao Animation.cpp

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -3,7 +3,7 @@
 int animationCount = 0;
 
 Animation::Animation(const int x_, const int y_, const int spriteWidth_,
-	const int spriteHeight_, const unsigned int numberOfImages_, const bool loop_) :
+					 const int spriteHeight_, const unsigned int numberOfImages_, const bool loop_) :
 	
 	ANIMATION_LIMIT(10),
 	x(x_),
@@ -15,8 +15,11 @@ Animation::Animation(const int x_, const int y_, const int spriteWidth_,
 	loop(loop_),
 	totalElapsedTime(0.0),
 	totalTime(0.0)
+
 {
+
 	this->numberOfImages = (numberOfImages_ == 0) ? 1 : numberOfImages_;
+
 }
 
 Animation::~Animation(){
@@ -40,29 +43,37 @@ void Animation::update(SDL_Rect& clip, const double dt_){
         animationCount += 1;
 
         if(Animation::animationCount <= this->numberOfImages){
+
 	        if(this->x < (int)ANIMATION_LIMIT){
+
 	        	this->x += 1;
+
 	        }
 	        else{
+
 	        	this->y += 1;
 	        	this->x = 0;
+
 	        }
     	}
 
     	if(endOfAnimation){
+
     		this->x= this->initialX;
     		this->y= this->initialY;
     		Animation::animationCount = 0;
+
     	}
     }
 
    	const int positionX_ = this->x * this->spriteWidth;
 	const int positionY_ = this->y * this->spriteHeight;
 	updateClip(clip, positionX_, positionY_);
+
 }
 
 void Animation::changeAnimation(const int x_, const int y_, const unsigned int numberOfImages_,
-	const bool loop_, const double totalTime_){
+								const bool loop_, const double totalTime_){
 
 	this->x = x_;
 	this->y = y_;
@@ -72,20 +83,27 @@ void Animation::changeAnimation(const int x_, const int y_, const unsigned int n
 	this->loop = loop_;
 	this->totalTime = totalTime_;
 	animationCount = 0;
+
 }
 
 void Animation::updateClip(SDL_Rect& clip, const int x_, const int y_){
+
 	clip.x = x_;
 	clip.y = y_;
 	clip.w = this->spriteWidth;
 	clip.h = this->spriteHeight;
+
 }
 
 int Animation::getCurrentFrame(){
+
 	return animationCount + 1;
+
 }
 
 void Animation::changeWidthHeight(int width_, int height_){
+
 	this->spriteWidth = width_;
 	this->spriteHeight = height_;
+
 }
