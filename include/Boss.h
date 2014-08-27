6d907782f4f9d31e0eed0d59da9bf2fcb12cfaff
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2014-08-27 19:48:31 -0300

    Commentary on Boss.h

--- include/Boss.h
+++ include/Boss.h
@@ -28,11 +28,13 @@ class Boss : public DynamicEntity {
 		/**
 		* The constructor.
 		*/
+
 		Boss(const double x_, const double y_, const std::string& path_, Player* const player_);
 
 		/**
 		* The destructor.
 		*/
+
 		virtual ~Boss();
 
 		/**
@@ -41,10 +43,25 @@ class Boss : public DynamicEntity {
 		* @param dt_ : Delta time. Time elapsed between one frame and the other, independent
 		* 	of processing speed.
 		*/
+
 		virtual void update(const double dt_);
 
+		/**
+		* Initializes the Boss's States
+		*/
+
 		void initializeStates();
+
+		/**
+		* Destroys the Boss's States
+		*/
+
 		void destroyStates();
+
+		/**
+		* Changes the Boss's States
+		*/
+
 		void changeState(const BStates state_);
 
 		/**
@@ -54,13 +71,36 @@ class Boss : public DynamicEntity {
 		* @param cameraX_ : The x position of the camera.
 		* @param cameraY_ : The y position of the camera.
 		*/
+
 		virtual void render(const double cameraX_, const double cameraY_);
+
+		/**
+		* Uses a Potion.
+		*/
 
 		void usePotion(const int strength_, const int distance_);
 		
+		/**
+		* Gets the Boss's animation.
+		*/
+
 		Animation* getAnimation();
+
+		/**
+		* Checks if the Boss is Dead
+		*/
+
 		bool isDead();
+
+		/**
+		* Sets the Boss Dead
+		*/
+
 		void setDead(bool isDead_);
+
+		/**
+		* Enumares Boss's skills
+		*/
 
 		enum BossSkills : uint8_t {
 			BS_MAGIC_SHIELD = 0,
@@ -71,26 +111,55 @@ class Boss : public DynamicEntity {
 			BS_FINAL_SPLENDOR
 		};
 
+		/** 
+		* Uses random skill.
+		*/
+
 		void randomSkill(const unsigned int index_);
+
+		/**
+		* Uses the teleport skill.
+		*/
+
 		bool teleport();
+
+		/**
+		* Uses the magic projectile skill.
+		*/
+
 		bool magicProjectile();
+
+		/**
+		* Uses the invoke wind skill.
+		*/
+
 		bool invokeWind();
+
+		/**
+		* Uses the ice prision skill.
+		*/
+
 		bool icePrision();
+
+		/**
+		* Uses the final splendor skill.
+		*/
+
 		bool finalSplendor();
 
-		unsigned int potionsLeft;
+		unsigned int potionsLeft; /**< Number of potions left */
 
-		bool sawPlayer;
-		std::vector<Potion*> potions;
+		bool sawPlayer;	/**< Boolean to check if the Boss has seen the player*/
+		std::vector<Potion*> potions; /**< Vector that contains the potions*/
 
-		unsigned int life;
-		bool hasShield;
-		bool canWalk;
-		Player* player;
+		unsigned int life; /**< Quantity of the Boss's life*/
+		bool hasShield; /**< Checks if the Boss has shield*/
+		bool canWalk; /**< Checks if the Boss can walk*/
+		Player* player; 
 		Animation* powerAnimation;
-		double powerX;
-		double powerY;
-		bool powerIsActivated;
+		double powerX; /**< X position of the skill*/
+		double powerY; /**< Y position of the skill*/
+		bool powerIsActivated; /**< checks if the power is activated*/
 		Sprite* power;
 		SDL_Rect powerClip;
 		SDL_RendererFlip powerFlip;
@@ -99,7 +168,17 @@ class Boss : public DynamicEntity {
 		SDL_Rect shieldClip;
 
 	private:
+
+		/**
+		* Updates de Bounding Box of the Boss.
+		*/
+
 		virtual void updateBoundingBox();
+
+		/**
+		* Handle collision with the Boss.
+		*/
+
 		virtual void handleCollision(std::array<bool, CollisionSide::SOLID_TOTAL> detections_);
 		
 		StateBoss* currentState;
