394b547205b936d7bb4016a45e577858714c2b61
simiaosimis <simiaosimis@hotmail.com>
2014-08-27 19:35:52 -0300

    Commenting methods on the main Class

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "Logger.h"
 #include "Configuration.h"
 
+
+// Main function, called in the beggining of the game.
 int main(int argc, char** argv){
 
 	// Unused.
@@ -10,18 +12,22 @@ int main(int argc, char** argv){
 
 	Log(DEBUG) << "Starting Dauphine...";
 
+	// Initialize the configuration values, such as width and height of the screen.
 	Configuration::initialize();
 
 	bool systemsInitialized = SDLWrapper::initialize();
 
 	if(systemsInitialized){
+		// Start the game.
 		Game::instance().runGame();
+		// Deallocate the instance of the game.
 		delete &Game::instance();
 	}
 	else{
 		Log(ERROR) << "System were not initialized.";
 	}
 
+	// Closes SDL used libraries.
 	SDLWrapper::close();
 
 	Log(DEBUG) << "Exiting Dauphine...";
