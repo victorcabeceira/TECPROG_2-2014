697fb32ef75ca8e5d8404b2a66202e97fb566995
victorcabeceira <victorfgcabeceira@gmail.com>
2014-08-29 14:07:43 -0300

    Applying style sheet and design to class EStateAerial.cpp

--- src/EStateAerial.cpp
+++ src/EStateAerial.cpp
@@ -2,11 +2,12 @@
 #include "Logger.h"
 
 void EStateAerial::enter(){
+
 	this->enemy->isGrounded = false;
+
 }
 
 void EStateAerial::exit(){
-
 }
 
 void EStateAerial::update(const double dt_){
@@ -15,22 +16,29 @@ void EStateAerial::update(const double dt_){
 
 	// Idle
 	if(this->enemy->isGrounded){
+
 		if(this->enemy->patrol){
+
 			this->enemy->changeState(Enemy::EStates::PATROLLING);
 			return;
+
 		}
 		else{
+
 			this->enemy->changeState(Enemy::EStates::IDLE);
 			return;
+
 		}
 	}
 
 	// Gravity
 	this->enemy->applyGravity();
+
 }
 
 EStateAerial::EStateAerial(Enemy* const enemy_) :
-	StateEnemy(enemy_)
-{
 
+	StateEnemy(enemy_)
+
+{
 }
