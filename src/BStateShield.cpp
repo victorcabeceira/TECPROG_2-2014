2ea4d7f46ba51539b5655227425be4b09d7adae4
victorcabeceira <victorfgcabeceira@gmail.com>
2014-08-28 22:56:42 -0300

    Applying style sheet and design to class BStateShield.cpp

--- src/BStateShield.cpp
+++ src/BStateShield.cpp
@@ -2,23 +2,27 @@
 #include "Logger.h"
 
 void BStateShield::enter(){
+
 	Log(DEBUG) << "STATE SHIELD BOSS";
 	this->boss->getAnimation()->changeAnimation(0, 0, 1, false, 0);
 	this->boss->hasShield = true;
+
 }
 
 void BStateShield::exit(){
-
 }
 
 void BStateShield::update(const double dt_){
+
 	((void)dt_); // Unused.
 
 	this->boss->changeState(Boss::BStates::IDLE);
+
 }
 
 BStateShield::BStateShield(Boss* const boss_) :
-	StateBoss(boss_)
-{
 
+	StateBoss(boss_)
+
+{
 }
