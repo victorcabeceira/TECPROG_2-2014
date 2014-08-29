6ffa87577b0e41b95b871ad439ee1e9d0c469447
simiaosimis <simiaosimis@hotmail.com>
2014-08-28 21:35:39 -0300

    Commenting methods on the util and boss, enemy and player state Class

--- src/StateBoss.cpp
+++ src/StateBoss.cpp
@@ -1,11 +1,13 @@
 #include "StateBoss.h"
 
+// Constructor of StateBoss, set the boss.
 StateBoss::StateBoss(Boss* const boss_) :
 	boss(boss_)
 {
 
 }
 
+// Destructor method, runs when the class is deallocated.
 StateBoss::~StateBoss(){
 	this->boss = nullptr;
 }
