20755e35a8a74318cf9132b46b81f54e1e2d9811
victorcabeceira <victorfgcabeceira@gmail.com>
2014-08-29 10:24:24 -0300

    Applying style sheet and design to class Crosshair.cpp

--- src/Crosshair.cpp
+++ src/Crosshair.cpp
@@ -1,31 +1,42 @@
 #include "Crosshair.h"
 
 Crosshair::Crosshair(const double x_, const double y_, const std::string& path_) :
+
 	Entity(x_, y_, path_),
 	activated(false)
-{
 
+{
 }
 
 Crosshair::~Crosshair(){
-
 }
 
 void Crosshair::update(const double dt_){
+
 	((void)dt_); // Unused.
+
 }
+
 //Render the crossrair of potions		
 void Crosshair::render(const double cameraX_, const double cameraY_){
+
 	if(this->activated && this->sprite != nullptr){
+
         const double dx = this->x - cameraX_;
         const double dy = this->y - cameraY_;
         this->sprite->render(dx, dy, nullptr, false, 0.0, nullptr, SDL_FLIP_HORIZONTAL);
+
     }
 }
+
 unsigned int Crosshair::getWidth(){
+
 	return this->width;
+
 }
 
 unsigned int Crosshair::getHeight(){
+
 	return this->height;
+
 }
