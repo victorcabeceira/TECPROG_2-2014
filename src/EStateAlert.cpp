c5872d9b025e9a1738f96fcdb83659e0ba56dfa0
victorcabeceira <victorfgcabeceira@gmail.com>
2014-08-29 14:09:34 -0300

    Applying style sheet and design to class EStateAlert.cpp

--- src/EStateAlert.cpp
+++ src/EStateAlert.cpp
@@ -2,12 +2,15 @@
 #include "Logger.h"
 
 void EStateAlert::enter(){
+
 	this->enemy->getAnimation()->changeAnimation(1, 0, 9, false, 0.9);
 	this->enemy->speed = 6.5;
 
 	if(enemy->life <= 0){
+
 		enemy->vy = 0;
 		enemy->changeState(Enemy::EStates::DEAD);
+
 	}
 }
 
@@ -20,32 +23,42 @@ void EStateAlert::update(const double dt_){
 	
 	// Aerial
 	if(!this->enemy->isGrounded){
+
 		this->enemy->changeState(Enemy::EStates::AERIAL);
 		return;
+
 	}
 
 	if(this->enemy->x - Enemy::px < 0.0){
+
 		this->enemy->vx += this->enemy->speed;
 
 	}
 	else{
+
 		this->enemy->vx -= this->enemy->speed;
+
 	}
 
 	if(abs(this->enemy->x - Enemy::px) < 100){
+
 		if(abs(this->enemy->y - Enemy::py) < 200){
+
 			this->enemy->changeState(Enemy::EStates::ATTACK);
+
 		}
 	}
 
 	if(!(abs(this->enemy->x - Enemy::px) < Enemy::alertRange*2 && abs(this->enemy->y - Enemy::py) < Enemy::alertRange)){
+
 		this->enemy->changeState(Enemy::EStates::PATROLLING);
 		return;
+	
 	}	
 }
 
 EStateAlert::EStateAlert(Enemy* const enemy_) :
+
 	StateEnemy(enemy_)
 {
-
 }
