4db0e4e445da85d84dbdded3a7b0547ac904aa64
Simião <simiaosimis@hotmail.com>
2014-09-06 22:15:16 -0300

    QuadTree style sheet

--- src/QuadTree.cpp
+++ src/QuadTree.cpp
@@ -6,13 +6,16 @@ http://gamedevelopment.tutsplus.com/tutorials/quick-tip-use-quadtrees-to-detect-
 */
 
 QuadTree::QuadTree(int level_, SDL_Rect bounds_) :
+
 	maxObjects(10),
 	maxLevels(5),
 	level(level_),
 	objects(),
 	bounds(bounds_),
 	nodes()
+
 {
+
 }
 
 QuadTree::~QuadTree(){
@@ -20,17 +23,22 @@ QuadTree::~QuadTree(){
 }
 
 void QuadTree::clear(){
+
 	this->objects.clear();
 
 	for(int i = 0; i < 4; i++){
+
 		if(this->nodes[i] != nullptr){
+
 			this->nodes[i]->clear();
 			this->nodes[i] = nullptr;
+
 		}
 	}
 }
 
 void QuadTree::split(){
+
 	const int subWidth = this->bounds.w / 2;
 	const int subHeight = this->bounds.h / 2;
 	const int x = this->bounds.x;
@@ -45,9 +53,11 @@ void QuadTree::split(){
 	this->nodes[1] = new QuadTree(this->level+1, rect1);
 	this->nodes[2] = new QuadTree(this->level+1, rect2);
 	this->nodes[3] = new QuadTree(this->level+1, rect3);
+
 }
 
 int QuadTree::getIndex(SDL_Rect rect_){
+
 	int index = -1;
 	double verticalMidpoint = this->bounds.x + (this->bounds.w / 2);
 	double horizontalMidpoint = this->bounds.y + (this->bounds.h / 2);
@@ -59,70 +69,105 @@ int QuadTree::getIndex(SDL_Rect rect_){
 
 	// Object can completely fit within the left quadrants
 	if (rect_.x < verticalMidpoint && rect_.x + rect_.w < verticalMidpoint) {
+
 		if (topQuadrant) {
+
 			index = 1;
+
 		}
+
 		else if (bottomQuadrant) {
+
 			index = 2;
+
 		}
 	}
+
 	// Object can completely fit within the right quadrants
 	else if (rect_.x > verticalMidpoint) {
+
 		if (topQuadrant) {
+
 			index = 0;
+
 		}
+
 		else if (bottomQuadrant) {
+
 			index = 3;
+
 		}
 	}
 
 	return index;
+
 }
 
 void QuadTree::insert(CollisionRect rect_){
+
 	if (nodes[0] != nullptr) {
+
 		int index = getIndex(rect_.rect);
 
 		if (index != -1) {
+
 			nodes[index]->insert(rect_);
 			return;
+
 		}
 	}
 
 	this->objects.push_back(rect_);
 
 	if ((int)this->objects.size() > this->maxObjects && level < this->maxObjects) {
+
 		if (nodes[0] == nullptr) {
+
 			split();
+
 		}
 
 		int i = 0;
 		while (i < (int)this->objects.size()) {
+
 			int index = getIndex(this->objects.at(i).rect);
+
 			if (index != -1) {
+
 				CollisionRect moveRect = this->objects.at(i);
 				this->objects.erase(this->objects.begin() + i);
 				nodes[index]->insert(moveRect);
+
 			}
+
 			else {
+
 				i++;
+
 			}
 		}
 	}
 }
 
 std::vector<CollisionRect> QuadTree::retrieve(std::vector<CollisionRect>& returnObjects_, SDL_Rect rect_){
+
 	int index = getIndex(rect_);
+
 	if (index != -1 && nodes[0] != nullptr) {
+
 		nodes[index]->retrieve(returnObjects_, rect_);
+
 	}
 
 	returnObjects_.insert(returnObjects_.end(), this->objects.begin(), this->objects.end());
 
 	return returnObjects_;
+
 }
 
 void QuadTree::setObjects(std::vector<CollisionRect>& objects_){
+
 	this->objects.clear();
 	this->objects = objects_;
+
 }
