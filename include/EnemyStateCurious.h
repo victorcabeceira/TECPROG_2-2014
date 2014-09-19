b3ddc3d839832138d1da5216dc07c62441676fa9
Victor <victorfgcabeceira@gmail.com>
2014-09-19 10:55:22 -0300

    Renaming Class EStateCurious.h and its variables

---
+++ include/EnemyStateCurious.h
@@ -0,0 +1,41 @@
+#ifndef INCLUDE_ESTATECURIOUS_H
+#define INCLUDE_ESTATECURIOUS_H
+
+#include "StateEnemy.h"
+
+class EnemyStateCurious : public StateEnemy {
+
+	public:
+	
+		/**
+		* The constructor.
+		* @param enemy_ : Reference to the Enemy.
+		*/
+		EnemyStateCurious(Enemy* const enemy_);
+		
+		/**
+		* The destructor.
+		*/
+		virtual ~EnemyStateCurious(){}
+
+		/**
+		* @see StateEnemy::enter
+		*/
+		virtual void enter();
+
+		/**
+		* @see StateEnemy::exit
+		*/
+		virtual void exit();
+
+		/**
+		* @see StateEnemy::update
+		*/
+		virtual void update(const double dt_);
+
+	private:
+		double timeElapsed;
+
+};
+
+#endif // INCLUDE_ESTATECURIOUS_H
