1df63841c5a231a5576acc28583702ff45cbb747
Victor <victorfgcabeceira@gmail.com>
2014-10-03 10:33:48 -0300

    Renaming BStateAttack.cpp

---
+++ src/BossStateAttack.cpp
@@ -0,0 +1,62 @@
+#include "BossStateAttack.h"
+#include "Logger.h"
+#include "Game.h"
+
+#include <cstdlib>
+#include <ctime>
+
+int attackIndex = 0;
+
+void BossStateAttack::enter(){
+
+	Log(DEBUG) << "STATE ATTACK BOSS";
+	this->boss->getAnimation()->changeAnimation(0, 0, 7, false, 1);
+	this->boss->sawPlayer = false;
+	attackIndex = rand()%4;
+
+}
+
+void BossStateAttack::exit(){
+
+	this->boss->canWalk = false;
+
+}
+
+//Update eh state of boss skills
+void BossStateAttack::update(const double deltaTime_){
+
+	((void)deltaTime_); // Unused.
+
+	if(attackIndex == 3){
+
+		this->boss->changEnemyState(Boss::BossStates::SHIELD);
+
+	}
+	else if(attackIndex == 0){
+
+		this->boss->changEnemyState(Boss::BossStates::TELEPORT);
+
+	}
+	else if(attackIndex == 2){
+
+		this->boss->changEnemyState(Boss::BossStates::ICEPRISION);
+
+	}
+	else if(attackIndex == 1){
+
+		this->boss->changEnemyState(Boss::BossStates::MAGICPROJECTILE);
+
+	}
+	else{
+	}
+}
+
+BossStateAttack::BossStateAttack(Boss* const boss_) :
+
+	StateBoss(boss_)
+
+{
+
+	srand(time(nullptr));
+	
+}
