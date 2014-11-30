1e94ec8ab04f43d3b295ebc3a021c2ed325e91e3
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2014-11-30 20:10:39 -0200

    Commentary on camera.cpp

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,6 +1,15 @@
 #include "Camera.h"
 #include "Configuration.h"
+/**
+* The camera class.
+* Contains all the relevant implementation relative to the camera.
+*/
 
+/**
+* The constructor.
+* Initializes all the attributes.
+* @param entity_ : The entity to centralize the camera on.
+*/
 Camera::Camera(Entity* const entity_):
 
     entity(entity_),
@@ -12,22 +21,36 @@ Camera::Camera(Entity* const entity_):
     /// @todo If the player changes the resolution, so should the clip. (?) (problem?)
 }
 
+/**
+* The destructor.
+*/
 Camera::~Camera(){
 }
 
+/**
+* Updates the camera.
+* @see Camera::updatePosition
+* Centralizes the player on the screen.
+*/
 void Camera::update(){
 
     updatePosition();
 
 }
 
+/**
+* @return The camera clip (an SDL_Rect).
+*/
 SDL_Rect& Camera::getClip(){
 
     return this->clip;
 
 }
 
-//This function updates the position of camera on level.
+/**
+* Updates the cameras position.
+* Based on the players position, change cameras position.
+*/
 void Camera::updatePosition(){
 
     this->clip.x = (this->entity->x + this->entity->getWidth() / 2) - (this->clip.w / 2);
@@ -61,13 +84,21 @@ void Camera::updatePosition(){
 
     }
 }
-//This function centralizes the camera.
+
+/**
+* Changes the entity to centralize upon.
+* @param entity_ : The entity to centralize the camera on.
+*/
 void Camera::centralizeOn(Entity* const entity_){
 
     this->entity = entity_;
 
 }
-//This function set the width and the height of the level. 
+
+/**
+* @param width_,height_ : Tells the camera what the width and height of the level is.
+* @see Level::update()
+*/ 
 void Camera::setLevelWidthHeight(const unsigned int width_, const unsigned int height_){
 
     this->levelWidth = width_;
